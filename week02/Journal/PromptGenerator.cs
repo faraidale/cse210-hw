810d9622b75ad8c93a1fc9c9f9c97f9e3de44008
faraidale <frwambiwa@gmail.com>
2026-03-14 19:42:08 +0200

    Add PromptGenerator class for random prompts

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+// PromptGenerator class - manages and provides random prompts
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What am I most grateful for today?",
+        "What challenge did I overcome today?"
+    };
+
+    // Get a random prompt from the list
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
