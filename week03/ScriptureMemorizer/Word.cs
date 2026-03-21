db5d8b60bfabe5e296237e1150e83400534b2212
faraidale <frwambiwa@gmail.com>
2026-03-21 16:30:19 +0200

    Implement Word class for scripture memorization

---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,46 @@
+using System;
+
+// Word class - represents a single word in the scripture
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    // Constructor
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    // Hide the word
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    // Show the word
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    // Check if word is hidden
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    // Get display text (either word or underscores)
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            return new string('_', _text.Length);
+        }
+        else
+        {
+            return _text;
+        }
+    }
+}
