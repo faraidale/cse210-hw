e2c8926b575e1daa3ad9555f74fcb223e963f0d7
faraidale <frwambiwa@gmail.com>
2026-04-11 16:25:07 +0200

    Add abstract Goal class for goal management

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,33 @@
+using System;
+
+// Base class for all goals
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    // Abstract methods must be overridden
+    public abstract int RecordEvent();
+    public abstract bool IsComplete();
+    public abstract string GetStringRepresentation();
+
+    // Virtual method can be overridden
+    public virtual string GetDetailsString()
+    {
+        string checkbox = IsComplete() ? "[X]" : "[ ]";
+        return $"{checkbox} {_shortName} ({_description})";
+    }
+
+    public string GetName()
+    {
+        return _shortName;
+    }
+}
