322efc61f9cef842a870313040df4aa818458270
faraidale <frwambiwa@gmail.com>
2026-04-14 16:59:38 +0200

    Updated Program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Create a list to store activities
+        List<Activity> activities = new List<Activity>();
+
+        // Create one of each type of activity
+        Running running = new Running("03 Nov 2022", 30, 3.0);
+        Cycling cycling = new Cycling("03 Nov 2022", 30, 20.0);
+        Swimming swimming = new Swimming("03 Nov 2022", 30, 20);
+
+        // Add activities to list
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        // Display summary for each activity
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
