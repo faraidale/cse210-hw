0d4c799e035896776e61490b4ac6a6ca4332665c
faraidale <frwambiwa@gmail.com>
2026-04-14 16:56:23 +0200

    Add Running class to track running activities

---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,28 @@
+using System;
+
+// Running activity - tracks distance
+public class Running : Activity
+{
+    private double _distance; // in miles
+
+    public Running(string date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (_distance / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / _distance;
+    }
+}
