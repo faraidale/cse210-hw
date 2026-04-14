1da1a4159372fc45d5eae94046fbbfe283862b23
faraidale <frwambiwa@gmail.com>
2026-04-14 16:57:50 +0200

    Add Swimming class for tracking swimming activities
    Implement Swimming activity to track laps and calculate distance, speed, and pace.

---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,29 @@
+using System;
+
+// Swimming activity - tracks laps
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        // Distance in miles: laps * 50 meters / 1000 / 1.609 (convert to miles)
+        return _laps * 50 / 1000.0 * 0.62;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
